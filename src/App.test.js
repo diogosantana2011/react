0b0c6479a123624190412c4eeb5c3aaf3a3d4c1e
diogosantana2011 <diegsan20@gmail.com>
2023-12-30 14:07:37 +0100

    (CHORE): Adding unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Game from './components/game';
+import MyButton from './components/button';
+import Profile  from '../src/components/profile.js';
+import AboutPage from './components/aboutPage.js';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/My New Home/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Component rendering tests', () => {
+  it('renders App component', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Diogo Santana/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  it('renders Game component', () => {
+    render(<Game />);
+    const linkElement = screen.getByText(/Next player:/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders My button component', () => {
+    render(<MyButton />);
+    const linkElement = screen.getByText(/I'm a button/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders Profile component', () => {
+    render(<Profile />);
+    const linkElement = screen.getByText(/Diogo Santana/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it('renders AboutPage component', () => {
+    render(<AboutPage />);
+    const linkElement = screen.getByText(/D'you like my smiling Pepe?/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
